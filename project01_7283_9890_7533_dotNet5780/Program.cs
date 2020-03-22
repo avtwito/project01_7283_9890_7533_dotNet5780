0125f9dcef95664464fd6b8644f98aff83a98a8a
Avishay <avtwito@g.jct.ac.il>
2020-03-22 22:02:42 +0200

    second commit

---
+++ project01_7283_9890_7533_dotNet5780/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project01_7283_9890_7533_dotNet5780
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
