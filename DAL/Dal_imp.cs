24b9af159354664289c0d2fce2bd848d2608e342
gurarieleviav <58116615+gurarieleviav@users.noreply.github.com>
2020-03-23 13:45:27 +0200

    adding more projects

---
+++ DAL/Dal_imp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    class Dal_imp
+    {
+        void SetGuestRequest(DS.GuestRequsetList)
+        {
+
+        }
+        void GetGuestRequest()
+        {
+
+        }
+        void updateGuestRequest()
+        {
+
+        }
+        void DeleteGuestRequest()
+        {
+
+        }
+    }
+}
